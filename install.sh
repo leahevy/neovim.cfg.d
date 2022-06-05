8c8ca443ffe4d7c81f9b73617324cc3570e3edaa
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-05 23:24:43 +0200

    Add deps

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt install ripgrep
     sudo apt install fd-find
     sudo apt install xsel
+    sudo apt install ncdu htop
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install ripgrep
     brew install fd
+    brew install ncdu htop
 fi
 
 pip install pynvim
