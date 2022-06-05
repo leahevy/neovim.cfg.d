71ca2643236044f13029d4c3fc7ae3e47138501c
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-05 22:23:01 +0200

    Fix: tabs to spaces

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 if [[ ! -e "$HOME/.config/nvim" ]]; then
-	ln -s "$(pwd)" "$HOME/.config/nvim"
+    ln -s "$(pwd)" "$HOME/.config/nvim"
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	sudo apt install ripgrep
-	sudo apt install fd-find
-	sudo apt install xsel
-	sudo apt install fonts-firacode
+    sudo apt install ripgrep
+    sudo apt install fd-find
+    sudo apt install xsel
+    sudo apt install fonts-firacode
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	brew install ripgrep
-	brew install fd
+    brew install ripgrep
+    brew install fd
 
-	brew tap homebrew/cask-fonts
-	brew install font-fira-code font-fira-mono font-fira-mono-for-powerline font-fira-sans
+    brew tap homebrew/cask-fonts
+    brew install font-fira-code font-fira-mono font-fira-mono-for-powerline font-fira-sans
 fi
 
 pip install pynvim
