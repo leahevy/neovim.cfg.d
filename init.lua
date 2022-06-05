9a43b94a95b8abc4cf996912082ad9f4df5be0df
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-05 21:11:20 +0200

    Configure: Init, options, keymaps, plugins

---
+++ init.lua
@@ -0,0 +1,4 @@
+require "leahevy.init"
+require "leahevy.options"
+require "leahevy.keymaps"
+require "leahevy.plugins"
--- install.sh
+++ install.sh
@@ -4,4 +4,13 @@ if [[ ! -e "$HOME/.config/nvim" ]]; then
 	ln -s "$(pwd)" "$HOME/.config/nvim"
 fi
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	sudo apt install ripgrep
+	sudo apt install fd-find
+	sudo apt install xsel
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	brew install ripgrep
+	brew install fd
+fi
+
 pip install pynvim
