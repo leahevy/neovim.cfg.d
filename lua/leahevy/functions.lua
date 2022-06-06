9ebbd5bb2e73a9331d2611238c1fafe512e0c599
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-06 14:03:08 +0200

    Improve handle url

--- lua/leahevy/functions.lua
+++ lua/leahevy/functions.lua
@@ -1,15 +1,13 @@
 -- Fix gx
-if vim.fn.has("mac") == 1 then
-    vim.cmd [[
-        function! HandleURL()
-          let s:uri = matchstr(getline("."), '[a-z]*:\/\/[^ >,;]*')
-          echo s:uri
-          if s:uri != ""
+vim.cmd [[
+    function! HandleURL()
+        let s:uri = matchstr(getline("."), '[a-z]*:\/\/[^ >,;]*')
+        echo s:uri
+        if s:uri != ""
             silent exec "!open '".s:uri."'"
-          else
+        else
             echo "No URI found in line."
-          endif
-        endfunction
-        map gx :call HandleURL()<cr>
-    ]]
-end
+        endif
+    endfunction
+    noremap gx :call HandleURL()<cr>
+]]
